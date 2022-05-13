14c9b1af1c3e213eef0496f5ac2133b824cdc1fd
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-05-12 21:41:46 -0500

    medelos_pregunta_y_respuestas

--- premioplatziapp/polls/models.py
+++ premioplatziapp/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# modelo de preguntas
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+# modelo de la respuesta
+class Choices(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE) # si elinimo la pregunta se elimina la respuestas
+    choice_text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0) # cuantos votos tiene cada resp, será un contador
+
