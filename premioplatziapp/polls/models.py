26f4bf6e4cff7ee27736234c812185d2cd55bb66
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-05-16 19:50:32 -0500

    agregando el metodo __str__ a los modelos

--- premioplatziapp/polls/models.py
+++ premioplatziapp/polls/models.py
@@ -1,10 +1,23 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 # modelo de preguntas
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    def __str__(self) -> str:
+        return self.question_text
+
+    # Metodo que retorna si una publicación es reciente o no,
+    # verifica que la pub_date este por delante de ayer a las misma hora del tiempo actual
+    # timedelta(days=1) objeto que retorna una diferencia de tiempo
+    def was_published_recently(self):
+        # timezone.now() - datetime.timedelta(days=1) para restar al tiempo actual un dia, nos vamos hacia atras en el tiempo
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 # modelo de la respuesta
 class Choice(models.Model):
