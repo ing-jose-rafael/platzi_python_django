19af9ec4a6828dad461daf0f9ed9a013ffdbb0c0
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-05-18 07:46:35 -0500

    remove auto_now=True in the model Question

--- premioplatziapp/polls/models.py
+++ premioplatziapp/polls/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 # modelo de preguntas
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published",auto_now=True)
+    pub_date = models.DateTimeField("date published")
 
     def __str__(self) -> str:
         return self.question_text
