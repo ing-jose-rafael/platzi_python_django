a22d56df85d60ad1c2034ebcf0c23f57ffa0db97
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-05-18 18:29:14 -0500

    funtion views details

--- premioplatziapp/polls/views.py
+++ premioplatziapp/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.template import loader
 
 from django.http import HttpResponse
@@ -18,8 +18,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Esta viendo la pregunta número {question_id}")
-
+    question = get_object_or_404(Question, pk=question_id)
+    # return HttpResponse(f"Esta viendo la pregunta número {question_id}")
+    context = {
+        'question': question,
+    }
+    return render(request,'polls/detail.html',context)
 
 def results(request, question_id):
     return HttpResponse(f"Esta viendo los resultados de la pregunta número {question_id}")
